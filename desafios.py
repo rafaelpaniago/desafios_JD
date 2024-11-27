c71c0b55538d5e33edc5c46d6617100592cc4cfe
Rafael Paniago <rafaelpaniago1@gmail.com>
2024-11-26 21:17:26 -0300

    Um desafio novo acrescentado

--- desafios.py
+++ desafios.py
@@ -1,6 +1,14 @@
 # Escreva um programa que some dois números inseridos pelo usuário.
 
-num1 = int(input('Digite um número -> '))
-num2 = int(input('Digite outro número -> '))
+# num1 = int(input('Digite um número -> '))
+# num2 = int(input('Digite outro número -> '))
 
-print(num1+num2)
+# print(num1+num2)
+
+
+# Escreva um programa que receba um número e calcule o resto da divisão desse úmero por 5
+
+num1 = int(input('Digite um número -> '))
+rest_5 = num1 % 5
+
+print(f'O resto da divisão do número {num1} por 5 é igual a: {rest_5}')
