696f75d953e0b860fbca77252d470796dc385f4b
Rafael Paniago <rafaelpaniago1@gmail.com>
2024-11-27 13:18:53 -0300

    Mais um desafio adicionado

--- desafios.py
+++ desafios.py
@@ -8,7 +8,23 @@
 
 # Escreva um programa que receba um número e calcule o resto da divisão desse úmero por 5
 
-num1 = int(input('Digite um número -> '))
-rest_5 = num1 % 5
+# num1 = int(input('Digite um número -> '))
+# rest_5 = num1 % 5
 
-print(f'O resto da divisão do número {num1} por 5 é igual a: {rest_5}')
+# print(f'O resto da divisão do número {num1} por 5 é igual a: {rest_5}')
+
+
+
+
+
+# Crie um programa que calculo a média de dois números flutuantes
+
+def media(a,b):
+    return (a+b)/2
+
+
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+
+print(f'A média dos números é: {media(num1,num2)}')
+
